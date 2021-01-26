3a009d45b182706a4e0683003e4e7b4400e10fcb
sERGIO <serg.ribmat@gmail.com>
2021-01-26 13:45:10 +0100

    add okhttpclient to retrofit object

--- app/src/main/java/sergio/ribera/random_cats/CatApiService.kt
+++ app/src/main/java/sergio/ribera/random_cats/CatApiService.kt
@@ -18,9 +18,11 @@ package sergio.ribera.random_cats
 
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
+import java.util.concurrent.TimeUnit
 
 
 const val ROOT_URL = "https://api.thecatapi.com/v1/images/"
@@ -30,10 +32,20 @@ private val moshi = Moshi.Builder()
     .add(KotlinJsonAdapterFactory())
     .build()
 
+private val okHttpClient = OkHttpClient.Builder()
+    .connectTimeout(6, TimeUnit.SECONDS)//time-out applied to create a TCP connection to the HTTP server.
+    .readTimeout(10, TimeUnit.SECONDS)//time passed between u connected and recieved the response
+    .writeTimeout(10, TimeUnit.SECONDS)//fail to write in time. Its for IO operations
+    .build()
+
 private val retrofit = Retrofit.Builder()
     .addConverterFactory(MoshiConverterFactory.create(moshi))
     .baseUrl(ROOT_URL)
+    .client(okHttpClient)
     .build()
+
+
+
 
 interface CatApiService {
     @GET("search")
--- app/src/main/java/sergio/ribera/random_cats/MainActivity.kt
+++ app/src/main/java/sergio/ribera/random_cats/MainActivity.kt
@@ -20,17 +20,17 @@ import android.Manifest
 import android.content.Context
 import android.net.ConnectivityManager
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
+import kotlinx.coroutines.*
+import kotlinx.coroutines.Dispatchers.Main
 import sergio.ribera.random_cats.databinding.ActivityMainBinding
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.launch
+import java.net.SocketTimeoutException
 
 
 class MainActivity : AppCompatActivity() {
@@ -39,6 +39,8 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
 
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -46,6 +48,8 @@ class MainActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         setUp()
+
+        //lookForInternetInTime(4)
 
     }
 
@@ -62,25 +66,39 @@ class MainActivity : AppCompatActivity() {
     fun catApiMethod(){
         permissionGiven = permissionBoolean()
         if(permissionGiven) {
-            showInternetAvailableLayout(true)
-            showNoInternetLayout(false)
+            //showInternetAvailableLayout(true)
+            //showNoInternetLayout(false)
 
             internetResponse()
         }else{
-            showInternetAvailableLayout(false)
-            showNoInternetLayout(true)
+            printNoInternetWithGlide()
+            //printImageWithGlide(R.drawable.ic_baseline_signal_wifi_off_24)
+            //showInternetAvailableLayout(false)
+            //showNoInternetLayout(true)
         }
     }
 
     fun internetResponse(){
+
         var viewModelJob = Job()
-        val uiScope = CoroutineScope(Dispatchers.Main +  viewModelJob)
+        val uiScope = CoroutineScope(Dispatchers.IO +  viewModelJob)
 
         uiScope.launch {
-            //aqui esta el objeto que se coge de internet
-            val imageUrlString = retrofitService.getProperties()[0].url
-            printImageWithGlide(imageUrlString)
-            viewModelJob.cancel()
+            try{
+                //aqui esta el objeto que se coge de internet
+                val imageUrlString = retrofitService.getProperties()[0].url
+                //aqui
+                withContext(Main){
+                    printImageWithGlide(imageUrlString)
+                }
+                uiScope.cancel()
+            }catch (e: SocketTimeoutException){
+                withContext(Main){
+                    showToast("The server is not responding")
+                }
+                uiScope.cancel()
+            }
+
         }
     }
 
@@ -106,6 +124,17 @@ class MainActivity : AppCompatActivity() {
             .into(findViewById(R.id.show_picture_imageview))
     }
 
+    private fun printNoInternetWithGlide() {
+        Glide.with(this)
+            .load("")
+            .apply(
+                RequestOptions()
+                    .placeholder(R.drawable.ic_baseline_signal_wifi_off_24)
+                    .error(R.drawable.ic_baseline_signal_wifi_off_24)
+            )
+            .into(findViewById(R.id.show_picture_imageview))
+    }
+
     fun showToast(text: String){
         Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
     }
@@ -126,5 +155,36 @@ class MainActivity : AppCompatActivity() {
         val activeNetworkInfo = connectivityManager.activeNetworkInfo
         return activeNetworkInfo != null && activeNetworkInfo.isConnected
     }
+
+    //hilo de ejecucion continuo
+    //si hay internet y noInternetLayout == visible, volver a bootn y imagen
+
+
+    fun lookForInternetInTime(seconds: Int){//parameter time in seconds
+        var viewModelJob = Job()
+        val uiScope = CoroutineScope(Dispatchers.IO +  viewModelJob)
+        val timeInMillis = fromSecondsToMillis(seconds)
+        uiScope.launch {
+            while (true){
+                checkShowCatAndButton()
+                Thread.sleep(timeInMillis)
+                Log.i("lookForInternetInTime", "Ha comprobado si hay internet o no")
+            }
+        }
+    }
+
+    suspend fun checkShowCatAndButton(){
+        if (isNetworkAvailable() && binding.noInternetLayout.visibility == View.VISIBLE){
+            withContext(Main){
+                showNoInternetLayout(false)
+                showInternetAvailableLayout(true)
+            }
+        }
+    }
+
+
+
+
+    fun fromSecondsToMillis(seconds: Int): Long = (seconds * 1000).toLong()
 
 }
